339172f423b86461d7c0ebe0eaffd26d9731bf8c
vickieee1812 <123375817+vickieee1812@users.noreply.github.com>
2023-04-19 11:03:19 +0530

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+    }
+    @RequestMapping
+    public String orderDetails(){
+
     }
 
 }
